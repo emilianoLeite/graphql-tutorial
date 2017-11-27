e47ccb0759643f787f7721d301490999b822555a
Emiliano Leite <emiliano.leite@codus.com.br>
2017-11-27 12:49:38 -0200

    Create Update and Delete mutations.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,4 +9,26 @@ Types::MutationType = GraphQL::ObjectType.define do
       Post.create(title: args[:title], author_id: args[:author_id])
     end
   end
+
+  field :updatePost, Types::PostType do
+    description "Updates a post"
+    argument :id, !types.ID
+    argument :title, types.String
+    argument :rating, types.Int
+
+    resolve ->(obj, args, ctx) do
+      post = Post.find(args[:id])
+      post.update(args.to_h)
+      post
+    end
+  end
+
+  field :deletePost, types.Boolean do
+    description "Deletes a post"
+    argument :id, !types.ID
+
+    resolve ->(obj, args, ctx) do
+      Post.find(args[:id]).destroy
+    end
+  end
 end
